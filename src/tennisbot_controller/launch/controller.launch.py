c7989af9843106945e616fce66f3449318faceef
Will Hsu <willysaixxx@gmail.com>
2026-08-31 21:50:48 +0800

    ros2_control

---
+++ src/tennisbot_controller/launch/controller.launch.py
@@ -0,0 +1,43 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import UnlessCondition, IfCondition
+
+def generate_launch_description():
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="True",
+    )
+
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager"
+        ]
+    )
+
+    # DiffDriveController = simple_controller + simple_velocity_controller
+    wheel_controller_spawner =  Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+                "diffdrive_controller", 
+                "--controller-manager", 
+                "/controller_manager"
+        ]
+    )
+
+
+    return LaunchDescription(
+        [
+            use_sim_time_arg,
+            joint_state_broadcaster_spawner,
+            wheel_controller_spawner
+        ]
+    )
