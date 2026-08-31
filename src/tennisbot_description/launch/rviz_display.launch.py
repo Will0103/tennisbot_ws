00bf42ab184f06fc9f31edf2bcb1200af26cf24b
Will Hsu <willysaixxx@gmail.com>
2026-08-31 20:33:58 +0800

    urdf

--- src/tennisbot_description/launch/rviz_display.launch.py
+++ src/tennisbot_description/launch/rviz_display.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
         executable="rviz2",
         name="rviz2",
         output="screen",
-        arguments=["-d", os.path.join(tennisbot_description_dir, "rviz", "display.rviz")],
+        arguments=["-d", os.path.join(tennisbot_description_dir, "rviz", "model.rviz")],
     )
 
     return LaunchDescription([
