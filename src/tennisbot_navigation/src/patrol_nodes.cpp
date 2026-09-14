591d77fea651db31b110edfe71735291cda22f97
Will Hsu <willysaixxx@gmail.com>
2026-09-14 19:42:38 +0800

    Add joystick control to patrol function

--- src/tennisbot_navigation/src/patrol_nodes.cpp
+++ src/tennisbot_navigation/src/patrol_nodes.cpp
@@ -4,6 +4,10 @@
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <sensor_msgs/msg/joy.hpp>
+
+#include <cmath>
+#include <vector>
 
 using FollowWaypoints = nav2_msgs::action::FollowWaypoints;
 using GoalHandleFollow = rclcpp_action::ClientGoalHandle<FollowWaypoints>;
@@ -16,22 +20,40 @@ public:
     PatrolNodesNode() : Node("patrol_node")
     {
         patrol_nodes_client_ = rclcpp_action::create_client<FollowWaypoints>(this, "follow_waypoints");
-        timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&PatrolNodesNode::timer_callback, this));
+        declare_parameter<int>("start_button", 3); //Y
+        start_button_ = this->get_parameter("start_button").as_int();
+        declare_parameter<int>("cancel_button", 2); //B
+        cancel_button_ = this->get_parameter("cancel_button").as_int();
+        joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, std::bind(&PatrolNodesNode::joy_callback, this, _1));
+
+        declare_parameter<std::vector<double>>("waypoints_x",{1.0, 1.0, -3.5, -3.5});
+        declare_parameter<std::vector<double>>("waypoints_y",{0.0, 4.0, 4.0, 0.0});
+        waypoints_x_ = get_parameter("waypoints_x").as_double_array();
+        waypoints_y_ = get_parameter("waypoints_y").as_double_array();
     }
 private:
-    void timer_callback()
+    void joy_callback(const sensor_msgs::msg::Joy &msg)
     {
-        
-        if (!patrol_nodes_client_->wait_for_action_server(std::chrono::seconds(1)))
-        {
-            RCLCPP_ERROR(get_logger(),
-                "FollowWaypoints server not available");
-            return;
+        if (msg.buttons[cancel_button_] == 1 && !previous_cancel_pressed_){
+            if (patrol_ongoing_){
+                if (patrol_goal_handle_)   patrol_nodes_client_->async_cancel_goal(patrol_goal_handle_);
+                else                       RCLCPP_WARN(get_logger(),"Patrol goal has not been accepted yet"); 
+            }    
         }
+        previous_cancel_pressed_ = msg.buttons[cancel_button_] == 1;
 
-        timer_->cancel();
-        send_goal();
-        
+        if (msg.buttons[start_button_] == 1 && !previous_start_pressed_ && !patrol_ongoing_){
+            if (!patrol_nodes_client_->action_server_is_ready())
+            {
+                RCLCPP_WARN(this->get_logger(),"FollowWaypoints action server is not ready");
+            }
+            else
+            {
+                RCLCPP_INFO(this->get_logger(),"Start partrol process!");
+                send_goal();
+            }
+        }
+        previous_start_pressed_ = msg.buttons[start_button_] == 1;
     }
 
     void send_goal()
@@ -40,15 +62,31 @@ private:
         goal.number_of_loops = 0;
         goal.goal_index = 0;
 
-        double yaw1 = atan2(6.0 - 3.0, -4.0 - 1.0);      // P1 → P2
-        double yaw2 = atan2(2.0 - 6.0, -4.0 - (-4.0));   // P2 → P3
-        double yaw3 = atan2(0.0 - 2.0, 0.0 - (-4.0));  // P3 → P4
-        double yaw4 = atan2(3.0 - 0.0, 1.0 - 0.0); // P4 → P1
+        // double yaw1 = atan2(6.0 - 3.0, -4.0 - 1.0);      // P1 → P2
+        // double yaw2 = atan2(2.0 - 6.0, -4.0 - (-4.0));   // P2 → P3
+        // double yaw3 = atan2(0.0 - 2.0, 0.0 - (-4.0));  // P3 → P4
+        // double yaw4 = atan2(3.0 - 0.0, 1.0 - 0.0); // P4 → P1
 
-        goal.poses.push_back(Pose2Stamp(1.0,3.0, yaw1));
-        goal.poses.push_back(Pose2Stamp(-4.0,6.0, yaw2));
-        goal.poses.push_back(Pose2Stamp(-4.0,2.0, yaw3));
-        goal.poses.push_back(Pose2Stamp(0.0, 0.0, yaw4));
+        // goal.poses.push_back(Pose2Stamp(1.0,3.0, yaw1));
+        // goal.poses.push_back(Pose2Stamp(-4.0,6.0, yaw2));
+        // goal.poses.push_back(Pose2Stamp(-4.0,2.0, yaw3));
+        // goal.poses.push_back(Pose2Stamp(0.0, 0.0, yaw4));
+
+
+        if (waypoints_x_.size() != waypoints_y_.size() ||  waypoints_x_.size() < 2)
+        {
+            RCLCPP_ERROR(
+                get_logger(),
+                "Invalid waypoint parameters");
+            return;
+        }
+
+        for (size_t i = 0; i < waypoints_x_.size(); i++)
+        {
+            size_t next = (i + 1) % waypoints_x_.size();
+            double yaw = atan2(waypoints_y_[next] - waypoints_y_[i], waypoints_x_[next] - waypoints_x_[i]);
+            goal.poses.push_back(Pose2Stamp(waypoints_x_[i], waypoints_y_[i], yaw));
+        }
 
         auto options = rclcpp_action::Client<FollowWaypoints>::SendGoalOptions();
         options.goal_response_callback = std::bind(&PatrolNodesNode::goal_response_callback, this, _1);
@@ -65,19 +103,23 @@ private:
             return;
         }
         patrol_goal_handle_ = goal_handle;
+        patrol_ongoing_ = true;
         RCLCPP_INFO(get_logger(),"Patrol goal accepted");
     }
 
     void result_callback(const GoalHandleFollow::WrappedResult &result)
     {
         auto status = result.code;
+        patrol_goal_handle_.reset();
         if (status == rclcpp_action::ResultCode::SUCCEEDED){
             RCLCPP_INFO(get_logger(),"Patrol goal succeed");
             send_goal();
         }else if (status == rclcpp_action::ResultCode::CANCELED){
             RCLCPP_INFO(get_logger(),"Patrol goal is canceled");
+            patrol_ongoing_ = false;
         }else{
             RCLCPP_INFO(get_logger(),"Patrol goal is aborted");
+            patrol_ongoing_ = false;
         }
     }
 
@@ -100,11 +142,15 @@ private:
     }
 
 
-
     rclcpp_action::Client<FollowWaypoints>::SharedPtr patrol_nodes_client_;
-    rclcpp::TimerBase::SharedPtr timer_;
     GoalHandleFollow::SharedPtr patrol_goal_handle_;
-
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;   
+    int cancel_button_, start_button_; 
+    bool patrol_ongoing_ = false;
+    bool previous_cancel_pressed_ = false;
+    bool previous_start_pressed_ = false;
+    std::vector<double> waypoints_x_;
+    std::vector<double> waypoints_y_;   
 };
 
 int main(int argc, char **argv)
