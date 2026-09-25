316872c7dc82cae72af505f67b2192ed18bc2763
Will Hsu <willysaixxx@gmail.com>
2026-09-25 14:40:04 +0800

    change usb port

--- src/tennisbot_bringup/launch/rplidar_a1_real.launch.py
+++ src/tennisbot_bringup/launch/rplidar_a1_real.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                'serial_port': '/dev/ttyUSB1',
                 'serial_baudrate': 115200,  # A1 / A2
                 # 'serial_baudrate': 256000, # A3
                 'frame_id': 'laser',
